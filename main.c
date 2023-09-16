94d8cf441b52acba0ea894ddb88f3f1cc9014e07
OKprincess <144990952+OKprincess@users.noreply.github.com>
2023-09-16 21:34:14 +0900

    Create main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  02
+//
+//  Created by ok sojoung on 2023/09/16.
+//
+
+#include <stdio.h>
+
+int main(int argc, char * argv[]) {
+    printf("Hello, Sookmyung Electronics!\n");
+    return 0;
+}
